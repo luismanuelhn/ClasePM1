0605c08eb13e38bd215cc019a84840757dc6b114
Luis <luismanuelhn@gmail.com>
2023-10-16 21:02:15 -0600

    subida con errores

--- app/src/main/java/com/example/pm013p2023/ActivityPhoto.java
+++ app/src/main/java/com/example/pm013p2023/ActivityPhoto.java
@@ -5,18 +5,28 @@ import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
+import androidx.core.content.FileProvider;
 
 import android.Manifest;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.os.Environment;
 import android.provider.MediaStore;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.Toast;
 
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 public class ActivityPhoto extends AppCompatActivity {
+    static final  int peticion_acceso_camara=101;
+    static final  int peticion_toma_fotografia=102;
+
 
     ImageView imageView;
     Button btntakefoto;
@@ -40,7 +50,7 @@ public class ActivityPhoto extends AppCompatActivity {
     }
     private  void permisos(){
       if(ContextCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.CAMERA)!= PackageManager.PERMISSION_GRANTED){
-          ActivityCompat.requestPermissions(this,new String[]{ Manifest.permission.CAMERA},101);
+          ActivityCompat.requestPermissions(this,new String[]{ Manifest.permission.CAMERA},peticion_acceso_camara);
       }else{
           Tomarfoto();
       }
@@ -49,7 +59,7 @@ public class ActivityPhoto extends AppCompatActivity {
     @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-        if(requestCode==101){
+        if(requestCode==peticion_acceso_camara){
             if(grantResults.length>0 && grantResults[0]==PackageManager.PERMISSION_GRANTED){
                 Tomarfoto();
             }
@@ -62,10 +72,50 @@ public class ActivityPhoto extends AppCompatActivity {
     private void Tomarfoto() {
     Intent intent=new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
     if(intent.resolveActivity(getPackageManager())!=null){
-          startActivityForResult(intent,102);
+          startActivityForResult(intent,peticion_toma_fotografia);
     }
 
     }
+
+    private File createImageFile() throws IOException, IOException {
+        // Create an image file name
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String imageFileName = "JPEG_" + timeStamp + "_";
+        File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+        File image = File.createTempFile(
+                imageFileName,  /* prefix */
+                ".jpg",         /* suffix */
+                storageDir      /* directory */
+        );
+
+        // Save a file: path for use with ACTION_VIEW intents
+        currentPhotoPath = image.getAbsolutePath();
+       return image;
+    }
+
+    private void dispatchTakePictureIntent() {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        // Ensure that there's a camera activity to handle the intent
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            // Create the File where the photo should go
+            File photoFile = null;
+            try {
+                photoFile = createImageFile();
+            } catch (IOException ex) {
+                // Error occurred while creating the File
+
+            }
+            // Continue only if the File was successfully created
+            if (photoFile != null) {
+                Uri photoURI = FileProvider.getUriForFile(this,
+                        "com.example.pmo120232p.fileprovider",
+                        photoFile);
+                takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
+                startActivityForResult(takePictureIntent, peticion_toma_fotografia);
+            }
+        }
+    }
+
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
